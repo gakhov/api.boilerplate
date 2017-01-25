046719063eafc6d736e0fe3024ab2a65b1cbef0a
Andrii Gakhov <gakhov@ferret-go.com>
2017-01-25 17:40:32 +0100

    Get rid of boostrap and use Python3 in virtualenv

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
-execfile('api/version.py')
+exec(open("api/version.py").read())
 
 setup(
     name="api.boilerplate",
@@ -13,30 +13,14 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Internet :: WWW/HTTP"
     ],
     packages=["api"],
-    include_package_data=True,
     extras_require=dict(
-        test=[
-            "pytest>=3.0.1,<4.0.0",
-            "pytest-cov==2.3.0",
-            "pytest-pep8==1.0.6",
-            "mock==2.0.0",
-            "sphinx==1.4.5",
-            "fabric==1.12.0",
-        ],
+        dev=open("requirements-dev.txt").readlines(),
     ),
-    install_requires=[
-        "futures>=3.0,<3.1",
-        "jsonschema==2.5.1",
-        "redis>=2.10,<2.11",
-        "requests[security]>=2.10,<3.0",
-        "strict_rfc3339==0.5",  # for jsonschema to validate date-time
-        "tornado>=4.0,<5.0",
-    ],
+    install_requires=open("requirements.txt").readlines(),
     zip_safe=False,
     entry_points={
         "console_scripts": [
